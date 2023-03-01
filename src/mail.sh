62bf135efb86f05a305840cb459e77f8c9ad0a13
sigmotoa <si.galvismotoa@gmail.com>
2023-03-01 16:08:00 -0500

    Installing ssmtp

--- src/mail.sh
+++ src/mail.sh
@@ -2,6 +2,7 @@ destiny=$GITLAB_USER_EMAIL
 subject="Result test"
 msg="Attached the results"
 attached=test-results.xml
+echo destiny
 mail=$(echo -e "Subject: $subject\n\n $msg")
 if [[ -f "$attached" ]]; then
 mail=$(echo -e "Subject: $subject\nContent-Type: text/plain; charset=utf-8\nContent-Disposition: attachment; filename=$(basename $attached)\n\n $msg" ; uuencode --base64 $attached $(basename $attached))

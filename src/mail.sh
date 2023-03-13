85f6fabfbf4d61f68e21b3329f9ff4dc98870abc
sigmotoa <si.galvismotoa@gmail.com>
2023-03-13 12:49:10 -0500

    Adding msmtp

--- src/mail.sh
+++ src/mail.sh
@@ -16,4 +16,4 @@ mail=$(echo -e "Subject: $subject\n\n $msg")
 if [[ -f "$attached" ]]; then
 mail=$(echo -e "Subject: $subject\nContent-Type: text/plain; charset=utf-8\nContent-Disposition: attachment; filename=$(basename $attached)\n\n $msg" ; uuencode --base64 $attached $(basename $attached))
 fi
-echo "$mail" | ssmtp $destiny
+echo "$mail" | msmtp $destiny

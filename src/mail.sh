7f4217624811cb7c2f24bd7e38901793a850d227
sigmotoa <si.galvismotoa@gmail.com>
2023-03-13 19:03:33 -0500

    Fixing config 9

--- src/mail.sh
+++ src/mail.sh
@@ -25,8 +25,10 @@ msg="Attached the results"
 attached=test-results.xml
 #echo $destiny
 #echo $GITLAB_USER_EMAIL
-mail=$(echo -e "Subject: $subject\n\n $msg")
-if [[ -f "$attached" ]]; then
-mail=$(echo -e "Subject: $subject\nContent-Type: text/plain; charset=utf-8\nContent-Disposition: attachment; filename=$(basename $attached)\n\n $msg" ; uuencode --base64 $attached $(basename $attached))
-fi
-echo "$mail" | msmtp $destiny
+
+#Temp
+#mail=$(echo -e "Subject: $subject\n\n $msg")
+#if [[ -f "$attached" ]]; then
+#mail=$(echo -e "Subject: $subject\nContent-Type: text/plain; charset=utf-8\nContent-Disposition: attachment; filename=$(basename $attached)\n\n $msg" ; uuencode --base64 $attached $(basename $attached))
+#fi
+#echo "$mail" | msmtp $destiny

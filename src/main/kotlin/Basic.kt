1669a542d9671dbd10b14348f5ad82025c87a2ec
sigmotoa <>
2023-08-15 11:38:14 -0500

    Solving first addition

--- src/main/kotlin/Basic.kt
+++ src/main/kotlin/Basic.kt
@@ -2,7 +2,7 @@ class Basic {
 
     //Make the sum of 2 numbers
     fun add_2_numbers(a: Int, b:Int): Int {
-        return 0
+        return a+b
     }
 
     //Make the sum of many numbers from a list of numbers
